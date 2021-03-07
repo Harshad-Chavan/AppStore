eda85f3d679a50b0de7ace430ceaa9b4ac9b3211
harshad <harshadchavan141@gmail.com>
2021-03-07 18:47:55 +0530

    changed database config

--- appstore/settings.py
+++ appstore/settings.py
@@ -94,12 +94,12 @@ WSGI_APPLICATION = 'appstore.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    # 'default': {
-    #     'ENGINE': 'django.db.backends.sqlite3',
-    #     'NAME': BASE_DIR / 'db.sqlite3',
-    # }
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 
-    'default':dj_database_url.config()
+    # 'default':dj_database_url.config()
 }
 
 
